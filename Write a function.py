33d36eb19380c471f261025c4e6bf18a97805ba3
Muhammad Catur Sentono Putro <60220651+anttonyh3h3@users.noreply.github.com>
2021-05-10 12:48:57 +0700

    Create Write a function.py

---
+++ Write a function.py
@@ -0,0 +1,9 @@
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return leap
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+    return leap
